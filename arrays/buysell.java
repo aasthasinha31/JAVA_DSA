e78ec15a8e6c63135e9209fbb1e38bead5db48c0
aasthasinha31 <aasthasinha201@gmail.com>
2026-05-27 13:38:10 +0530

    buy_or_selling_stocks

---
+++ arrays/buysell.java
@@ -0,0 +1,23 @@
+public class buysell {
+    public static int stocks(int prices[]) {
+        int buyprice = Integer.MAX_VALUE;
+        
+        int maxprofit = 0;
+         for(int i = 0 ; i<prices.length ; i++){
+            if(buyprice < prices[i]){
+                int profit = prices[i] - buyprice;
+                maxprofit = Math.max(profit, maxprofit);
+            }
+        else{ buyprice = prices[i];
+
+        }
+        
+       }
+       return maxprofit;
+    }
+    public static void main(String[] args) {
+        int prices[] = {1,3,4,2,3,6};
+        System.out.println(stocks(prices));
+    }
+    
+}
