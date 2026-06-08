71058579facb658efd8e6a2415cfea25ea3d2c1d
aasthasinha31 <aasthasinha201@gmail.com>
2026-06-08 12:55:05 +0530

    pairing_problem

---
+++ recursion/pairing.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class pairing {
+    public static int pair(int n){
+        if(n == 1 || n ==2){
+            return n;
+        }
+
+
+        int single = pair(n-1);
+        int pairs = single * pair(n-2);
+        int totalways = single + pairs;
+        return totalways;
+    }
+    public static void main(String[] args) {
+        System.out.println(pair(3));
+    }
+    
+}
