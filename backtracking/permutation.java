31457dc49e3e8f958e191b1b899199215a70c97e
aasthasinha31 <aasthasinha201@gmail.com>
2026-07-22 19:57:09 +0530

    permutation

---
+++ backtracking/permutation.java
@@ -0,0 +1,23 @@
+package backtracking;
+public class permutation {
+    public static void permutations(String str , String ans ) {
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+
+
+
+        for(int i = 0 ; i < str.length() ; i++){
+            char current = str.charAt(i);
+            String newstr= str.substring(0,i) + str.substring(i+1);
+            permutations(newstr, ans+current);
+        }
+        
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        permutations(str, "");
+    }
+    
+}
