9caf379857893ef4469b760a5a412b3ac3ddadff
aasthasinha31 <aasthasinha201@gmail.com>
2026-07-27 23:57:49 +0530

    Addinmid_LinkedList

---
+++ linkedlist/addinmid.java
@@ -0,0 +1,90 @@
+package linkedlist;
+
+public class addinmid {class Node{
+
+        int data ;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+
+        }
+
+       
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addmid(int idx , int data){
+        if(idx == 0){
+        addfirst(data);
+           return;
+        }
+
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+        while(i<idx-1){
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+       }
+    public void addlast(int data){
+        
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        tail = newNode ;
+    }
+    public void addfirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+       newNode.next = head;
+
+        head = newNode;
+
+    }
+
+
+    
+
+    public void print(){
+        if(head == null){
+           System.out.println("empty");
+            return;
+        }
+        Node temp = head;
+        while(temp!= null){
+            System.out.print(temp.data + " -> ");
+            
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        addinmid LL = new addinmid();
+        LL.addfirst(1);
+         LL.print();
+        LL.addfirst(5);
+        
+         LL.print();
+        LL.addlast(9);
+         LL.print();
+
+         LL.addmid(2,3);
+          LL.print();
+        
+
+    }
+    
+}
